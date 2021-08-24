c8e766571d04662e7447372234f2f55e5bb8f1f0
jayanthjaya <jayanthjaya567@gmail.com>
2021-08-24 18:05:45 +0530

    first commit

---
+++ src/DAY1/HellowWorld.java
@@ -0,0 +1,13 @@
+package DAY1;
+
+import java.util.Scanner;
+
+public class HellowWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new  Scanner("System.in");
+		System.out.printf("Hello World");
+	}
+
+}
